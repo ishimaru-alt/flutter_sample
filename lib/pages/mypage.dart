ee4ca15f90d847764df14a6b53c3007ff17da806
maru <ntiadori0821@gmail.com>
2021-06-07 14:24:42 +0900

    no message

--- lib/pages/mypage.dart
+++ lib/pages/mypage.dart
@@ -1,6 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-class MyPage extends StatelessWidget {
+void main() async {
+ WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // ここ大事！
+  runApp(MyPage());
+}
+
+class MyPage extends StatefulWidget{
+  LoginPage createState()=> LoginPage();
+}
+
+class LoginPage extends State<MyPage>{
+  // 入力されたメールアドレス
+  String newUserEmail = "";
+  // 入力されたパスワード
+  String newUserPassword = "";
+  // 登録・ログインに関する情報を表示
+  String infoText = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,6 +34,11 @@ class MyPage extends StatelessWidget {
                 decoration: InputDecoration(
                   labelText: 'Email',
                   ),
+                onChanged: (String value){
+                  setState((){
+                    newUserEmail = value;
+                  });
+                },
               ),
               TextField(
                 //パスワードの場合
@@ -25,12 +48,33 @@ class MyPage extends StatelessWidget {
                 decoration: InputDecoration(
                   labelText: 'Password' ,
                   ),
+                onChanged: (String value) {
+                  setState(() {
+                    newUserPassword = value;
+                  });
+                },
               ),
               ElevatedButton(
-                onPressed: (){},
-                child: 
-                  Text('Login')
+                onPressed: () async {
+                try{
+                  final FirebaseAuth auth = FirebaseAuth.instance;
+                  final UserCredential result = await auth.createUserWithEmailAndPassword(email: newUserEmail, password: newUserPassword,);
+
+                  final User user = result.user!;
+                  setState((){
+                    infoText = '登録OK:${user.email}';
+                  });
+                } catch(e) {
+                  setState((){
+                  infoText = '登録NG：${e.toString()}';
+                  });
+                }
+                },
+                child:
+                  Text('Login'),
               ),
+              const SizedBox(height:8),
+              Text(infoText),
               ElevatedButton(
                 onPressed: (){
                 },
